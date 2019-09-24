c2e29c092da69a8d00dbdc35635e63b96de76086
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-24 10:01:34 +0500

    Update xSW02.cpp

--- src/xSW02.cpp
+++ src/xSW02.cpp
@@ -465,8 +465,8 @@ uint8_t xSW02::setGasHeater(uint8_t set_point)
 {
 	uint8_t res_heat_x = 0;
 	double var1 = 0.0, var2 = 0.0, var3 = 0.0, var4 = 0.0, var5 = 0.0;
-	uint16_t par_g1 = ((uint16_t) xCore.read8(BME680_I2C_ADDRESS, 0xEC) << 8) | xCore.read8(BME680_I2C_ADDRESS, 0xEB);
-	uint8_t  par_g2 = xCore.read8(BME680_I2C_ADDRESS, 0xED);
+	uint16_t par_g1 = xCore.read8(BME680_I2C_ADDRESS, 0xED);
+	uint8_t  par_g2 = ((uint16_t) xCore.read8(BME680_I2C_ADDRESS, 0xEC) << 8) | xCore.read8(BME680_I2C_ADDRESS, 0xEB);
 	uint8_t  par_g3 = xCore.read8(BME680_I2C_ADDRESS, 0xEE);
 	uint8_t  res_heat_range = (xCore.read8(BME680_I2C_ADDRESS, 0x02) & 0x30) >> 4;
 	uint8_t res_heat_val = xCore.read8(BME680_I2C_ADDRESS, 0x00);
@@ -475,7 +475,7 @@ uint8_t xSW02::setGasHeater(uint8_t set_point)
 	var3 = (double)par_g3 / 1024.0;
 	var4 = var1 * (1.0 + (var2 * (double)set_point));
 	var5 = var4 + (var3 * 25.0); // use 25 C as ambient temperature
-	res_heat_x = (uint8_t)(((var5 * (4.0 / (4.0 * (double)res_heat_range))) - 25.0) * 3.4 / ((res_heat_val * 0.002) + 1));
+	res_heat_x = (uint8_t)(3.4 * ((var5 * (4.0 / (4.0 + (double)res_heat_range)) * (1.0/(1.0 +((double)res_heat_val * 0.002)))) - 25));
 	return res_heat_x;
 }
 
